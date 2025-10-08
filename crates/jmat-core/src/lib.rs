bfaa2d49c4a29d9b0baa05d2e48c566a007ec0bb
Jeremy Ianne <jviguytwo2@gmail.com>
2025-10-08 17:04:33 -0400

    working exprs and a lot. Going to convert them into an arena allocation instead of a tree in memory

--- crates/jmat-core/src/lib.rs
+++ crates/jmat-core/src/lib.rs
@@ -1,6 +1,10 @@
 pub mod matrix;
-mod expr;
-mod scalar;
+pub mod expr;
+pub mod scalar;
+pub mod types;
 
 pub use matrix::dmatrix::DMatrix;
 pub use matrix::smatrix::SMatrix;
+pub use expr::Expr;
+pub use scalar::Scalar;
+pub use types::*;
---
+++ crates/jmat-core/src/types.rs
@@ -0,0 +1,8 @@
+﻿use crate::DMatrix;
+use crate::expr::Expr;
+
+#[derive(Debug, Clone)]
+pub enum JmatValue {
+    Concrete(DMatrix<f64>),
+    Symbolic(DMatrix<Expr>),
+}
